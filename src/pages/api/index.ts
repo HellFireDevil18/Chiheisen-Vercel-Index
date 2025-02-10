669de7d15b8bc121b22766463f0db45eaf549988
地獄の火の悪魔 <priyanshudey18@gmail.com>
2025-02-10 21:40:54 +0530

    Auto Refresh Token

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 import apiConfig from '../../../config/api.config'
 import siteConfig from '../../../config/site.config'
-import { revealObfuscatedToken } from '../../utils/oAuthHandler'
+import { getAuthPersonInfo,  revealObfuscatedToken } from '../../utils/oAuthHandler'
 import { compareHashedToken } from '../../utils/protectedRouteHandler'
 import { getOdAuthTokens, storeOdAuthTokens } from '../../utils/odAuthTokenStore'
 import { runCorsMiddleware } from './raw'
@@ -159,14 +159,23 @@ export async function checkAuthRoute(
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // If method is POST, then the API is called by the client to store acquired tokens
   if (req.method === 'POST') {
-    const { obfuscatedAccessToken, accessTokenExpiry, obfuscatedRefreshToken } = req.body
-    const accessToken = revealObfuscatedToken(obfuscatedAccessToken)
-    const refreshToken = revealObfuscatedToken(obfuscatedRefreshToken)
+    const { accessToken, accessTokenExpiry, refreshToken } = await req.json()
 
+    
     if (typeof accessToken !== 'string' || typeof refreshToken !== 'string') {
       res.status(400).send('Invalid request body')
       return
     }
+
+    // verify identity of the authenticated user with the Microsoft Graph API
+    const { data, status } = await getAuthPersonInfo(accessToken)
+    if (status !== 200) {
+      return new Response("Non-200 response from Microsoft Graph API", { status: 500 })
+    }
+    if (data.userPrincipalName !== siteConfig.userPrincipalName) {
+      return new Response("Do not pretend to be the owner!", { status: 403 })
+    }
+
 
     await storeOdAuthTokens({ accessToken, accessTokenExpiry, refreshToken })
     res.status(200).send('OK')
