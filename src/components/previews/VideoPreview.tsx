90132b8949d4a461fe6697a7957ffb08d1301eab
地獄の火の悪魔 <priyanshudey18@gmail.com>
2025-09-30 21:59:10 +0530

    Relax Plyr prop types in VideoPreview component
    Changed Plyr prop types from strict Plyr.Options and Plyr.SourceInfo to 'any' to address type compatibility issues. This allows more flexibility when passing options and source props to the Plyr component.

--- src/components/previews/VideoPreview.tsx
+++ src/components/previews/VideoPreview.tsx
@@ -63,7 +63,7 @@ const VideoPlayer: FC<{
     poster: thumbnail,
     tracks: [{ kind: 'captions', label: videoName, src: '', default: true }],
   }
-  const plyrOptions: Plyr.Options = {
+  const plyrOptions: any = {
     ratio: `${width ?? 16}:${height ?? 9}`,
     fullscreen: { iosNative: true },
   }
@@ -71,7 +71,7 @@ const VideoPlayer: FC<{
     // If the video is not in flv format, we can use the native plyr and add sources directly with the video URL
     plyrSource['sources'] = [{ src: videoUrl }]
   }
-  return <Plyr id="plyr" source={plyrSource as Plyr.SourceInfo} options={plyrOptions} />
+  return <Plyr id="plyr" source={plyrSource as any} options={plyrOptions} />
 }
 
 const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
